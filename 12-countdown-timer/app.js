55b02ce298871338442e0ef6599720199091a90c
JoeTech <oyinjoe23@gmail.com>
2022-01-21 22:31:53 +0000

    Update app.js

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,13 +26,13 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 let tempDate = new Date();
-let tempYear = tempDate.getDate.getFullYear();
+let tempYear = tempDate.getFullYear();  
 let tempMonth = tempDate.getMonth();
-let temp = tempDate.getDate();
+let tempDay = tempDate.getDate();
 
-// let futureDate = new Date(2022, 3, 23, 11, 30, 0);
+let futureDate = new Date(2022, 3, 23, 11, 30, 0);
 // console.log(futureDate);
-const futureDate = new Date(tempYear,tempMonth,tempDay + 10, 11, 30, 0);
+// const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -45,6 +45,7 @@ giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${
 
 // future time in ms
 const futureTime = futureDate.getTime();
+
 function getRemainingTime() {
   const today = new Date().getTime();
   const t = futureTime - today;
@@ -69,6 +70,7 @@ function getRemainingTime() {
 
   // set values array;
   const values = [days, hours, minutes, seconds];
+
   function format(item) {
     if (item < 10) {
       return (item = `0${item}`);
@@ -78,7 +80,7 @@ function getRemainingTime() {
   items.forEach(function (item, index) {
     item.innerHTML = format(values[index]);
   });
-  if (t< 0) {
+  if (t < 0) {
     clearInterval(countdown);
     deadline.innerHTML = `<h4class="expired"> sorry, this giveaway has expired</h4>`;
   }
