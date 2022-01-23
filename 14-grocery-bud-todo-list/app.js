a497e60e57ab8a528961235efd1a81cad7a696ef
JoeTech <oyinjoe23@gmail.com>
2022-01-23 06:14:15 +0000

    Update app.js

--- 14-grocery-bud-todo-list/app.js
+++ 14-grocery-bud-todo-list/app.js
@@ -1,11 +1,11 @@
 // ****** SELECT ITEMS **********
-const alert = document.querySelector('.alert');
-const form = document.querySelector('.grocery-form');
-const grocery = document.getElementById('grocery');
-const submitBtn = document.querySelector('.submit-btn');
-const container = document.querySelector('.grocery-container');
-const list = document.querySelector('.grocery-list');
-const clearBtn = document.querySelector('.clear-btn');
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 // edit option
 let editElement;
 let editFlag = false;
