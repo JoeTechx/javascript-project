0f9ac47d341f4f2fa70423451dd559b7a4ef9247
Oyinloye Joe <oyinjoe23@gmail.com>
2022-04-12 18:14:57 -0700

    little change

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -2,29 +2,25 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
-// console.log(question);
-const btn = question.querySelector(".question-btn");
-// console.log(btn);
-btn.addEventListener("click", function () {
-
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", function () {
     questions.forEach(function (item) {
-        if (item !== question) {
-          item.classList.remove("show-text");
-        }
-      });
-        question.classList.toggle("show-text");
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
 });
-});
-
-
-
 
 // traversing the dom
 //  const btns = document.querySelectorAll(".question-btn");
 
 // btns.forEach(function (btn) {
 // btn.addEventListener("click", function (e) {
-//   const question = e.currentTarget.parentElemment.parentElemment;
+//   const question = e.currentTarget.parentElement.parentElement;
 
 //   question.classList.toggle("show-text");
 // });
